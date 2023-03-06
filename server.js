26bb44cdfa92f0f87db9904921eec712f8faa212
Nrochester <123398837+Nrochester@users.noreply.github.com>
2023-03-06 15:56:47 -0500

    practice express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
 
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
+
+app.use(express.static("public"));
